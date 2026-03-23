020d771d0fabb092dc321dfddc797c8426dc71be
IVERSON <ivertaracontre@gmail.com>
2026-03-23 14:29:30 -0500

    prueba de los estilos de tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,12 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
-import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: "https://iversontc.github.io",
-  base: "/portafolio/",
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  experimental: {
-    contentIntellisense: true,
-  },
+  base: "/portafolio",
+ 
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
