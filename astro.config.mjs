067b2f4bcac3a5c79cd451ad5df9f4f6fb7b7421
IVERSON <ivertaracontre@gmail.com>
2026-03-23 15:01:08 -0500

    prueba de los estilos de tailwind v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://iversontc.github.io",
-  base: "/portafolio",
- 
 });
